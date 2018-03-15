07ae5960c8650f197ab1890e1671c74a7a8820e4
psouki <psoukic@gmail.com>
2018-03-15 13:50:51 -0300

    Query and change interceptors applied

--- WebService.DataService/JokeService.svc.cs
+++ WebService.DataService/JokeService.svc.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Data.Services;
 using System.Data.Services.Common;
 using System.Linq;
+using System.Linq.Expressions;
 using System.ServiceModel.Web;
 using System.Web;
 
@@ -23,6 +24,34 @@ namespace WebService.DataService
             config.SetEntitySetAccessRule("Joke", EntitySetRights.All);
             config.UseVerboseErrors = true;
             config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+            config.SetServiceOperationAccessRule("GradeOrdered", ServiceOperationRights.All);
         }
+
+        //A custom method with the LINQ query provided instead of the query string
+        [WebGet(UriTemplate = "GradeOrdered")]
+        public IQueryable<Joke> GradeOrdered()
+        {
+            return CurrentDataSource.Joke.OrderBy(j => j.Grade);
+        }
+
+        //With this interceptor applied the Joke entity only will return jokes with grades
+        // greater than 2. It is used to apply default filters.
+        [QueryInterceptor("Joke")]
+        public Expression<Func<Joke, bool>> ApplyGradeFilter()
+        {
+            return j => j.Grade > 2;
+        }
+
+        // This will intercept every change operation and apply this rule.
+        [ChangeInterceptor("Joke")]
+        public void UpdateCustomer(Joke joke, UpdateOperations operations)
+        {
+            if (string.IsNullOrWhiteSpace(joke.Question))
+            {
+                throw new DataServiceException("The question is required !");
+            }
+        }
+
+
     }
 }
